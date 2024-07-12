38b841b7475d50a68b9cbc3a8cfd5c9eab4efc18
TheBitFossil <8717392-gearguard@users.noreply.gitlab.com>
2024-07-12 12:30:45 +0200

    add facing direction update facing with flipbooks

--- Source/ZombieSurvivor/TopDownCharacter.cpp
+++ Source/ZombieSurvivor/TopDownCharacter.cpp
@@ -16,25 +16,100 @@ ATopDownCharacter::ATopDownCharacter()
 
 	FlipBookComponent = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("FlipbBook Comp"));
 	FlipBookComponent->SetupAttachment(RootComponent);
-	
 }
 
 void ATopDownCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	if(APlayerController* PC = static_cast<APlayerController*>(GetController()))
+	if(APlayerController* PC = static_cast<APlayerController*>(Controller))
 	{
-		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
 		{
 			Subsystem->AddMappingContext(IMC_Default, 0);
 		}		
 	}
 }
 
+EDirectionFacing ATopDownCharacter::GetDirectionFacing(const FVector2D& Value)
+{
+	if(Value.X > 0.f)
+	{
+		DirectionFacing = EDirectionFacing::RIGHT;
+	}
+	else if(Value.X < 0.f)
+	{
+		DirectionFacing = EDirectionFacing::LEFT;
+	}
+	else if(Value.Y > 0.f)
+	{
+		DirectionFacing = EDirectionFacing::UP;
+	}
+	else if(Value.Y < 0.f)
+	{
+		DirectionFacing = EDirectionFacing::DOWN;
+	}
+
+	return DirectionFacing;
+}
+
+void ATopDownCharacter::ChangeFlipBookAnimation()
+{
+	// Either Running or Idling
+	TArray<UPaperFlipbook*> NextFlipBook;
+	if(Direction.Length() != 0.f)
+	{
+		NextFlipBook = FB_Run;
+	}
+	else
+	{
+		NextFlipBook = FB_Idle;
+	}
+
+	if(!NextFlipBook.IsEmpty() && FlipBookComponent != nullptr)
+	{
+		switch (GetDirectionFacing(Direction))
+		{
+			case EDirectionFacing::UP:
+				FlipBookComponent->SetFlipbook(NextFlipBook[0]);
+				break;
+			case EDirectionFacing::DOWN:
+				FlipBookComponent->SetFlipbook(NextFlipBook[1]);
+				break;
+			case EDirectionFacing::RIGHT:
+				FlipBookComponent->SetFlipbook(NextFlipBook[2]);
+				break;
+			case EDirectionFacing::LEFT:
+				FlipBookComponent->SetFlipbook(NextFlipBook[3]);
+				break;
+		}
+	}
+}
+
 void ATopDownCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if(bCanMove)
+	{
+		if(Direction.Length() != 0.f)
+		{
+			// Check if we need to normalize due to incorrect speed of two directions
+			if(Direction.Length() > 1.f)
+			{
+				Direction.Normalize();
+			}
+		
+			FVector2d Velocity = Direction * MoveSpeed * GetWorld()->GetDeltaSeconds();
+			FVector Location = GetActorLocation();
+			FVector NewLocation = Location + FVector(Velocity.X, 0.f, Velocity.Y);
+	
+			SetActorLocation(NewLocation);
+		}
+
+		ChangeFlipBookAnimation();
+	}
 }
 
 void ATopDownCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -43,25 +118,33 @@ void ATopDownCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 
 	if(UEnhancedInputComponent* InputComponent = static_cast<UEnhancedInputComponent*>(PlayerInputComponent))
 	{
-		InputComponent->BindAction(IA_Move, ETriggerEvent::Triggered, this, &ATopDownCharacter::Move);
+		InputComponent->BindAction(IA_Move, ETriggerEvent::Triggered, this, &ATopDownCharacter::MoveTriggered);
+		InputComponent->BindAction(IA_Move, ETriggerEvent::Completed, this, &ATopDownCharacter::MoveCompleted);
+
+		InputComponent->BindAction(IA_Shoot, ETriggerEvent::Started, this, &ATopDownCharacter::Shoot);
+		InputComponent->BindAction(IA_Shoot, ETriggerEvent::Triggered, this, &ATopDownCharacter::Shoot);
 	}
-	
 }
 
-void ATopDownCharacter::Move(const FInputActionValue& Value)
+void ATopDownCharacter::MoveTriggered(const FInputActionValue& Value)
 {
 	const FVector2d InputAction = Value.Get<FVector2d>();
 
-	if(InputAction.Length() != 0.f)
+	if(bCanMove)
 	{
 		// Check direction we are moving in
-		FVector2d Direction = InputAction;
-		// Add movement in this direction
-		FVector2d Velocity = Direction * GetWorld()->GetDeltaSeconds() * MoveSpeed;
-		FVector Location = GetActorLocation();
-		FVector NewLocation = Location + FVector(Velocity.X, 0.f, Velocity.Y);
-		
-		SetActorLocation(NewLocation);
+		Direction = InputAction;
 	}
 }
 
+void ATopDownCharacter::MoveCompleted(const FInputActionValue& Value)
+{
+	const FVector2d InputAction = Value.Get<FVector2d>();
+
+	Direction = FVector2D::Zero();
+}
+
+void ATopDownCharacter::Shoot(const FInputActionValue& Value)
+{
+	
+}
--- Source/ZombieSurvivor/TopDownCharacter.h
+++ Source/ZombieSurvivor/TopDownCharacter.h
@@ -9,6 +9,15 @@
 #include "GameFramework/Pawn.h"
 #include "TopDownCharacter.generated.h"
 
+UENUM()
+enum class EDirectionFacing : uint8
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
 UCLASS()
 class ZOMBIESURVIVOR_API ATopDownCharacter : public APawn
 {
@@ -34,13 +43,47 @@ public:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Input)
 	float MoveSpeed {200.f};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Input)
+	bool bCanMove {true};
+
+	// UP/ DOWN / RIGHT / LEFT
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=FlipBooks)
+	TArray<TObjectPtr<UPaperFlipbook>> FB_Idle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=FlipBooks)
+	TArray<TObjectPtr<UPaperFlipbook>> FB_Walk;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=FlipBooks)
+	TArray<TObjectPtr<UPaperFlipbook>> FB_Run;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=FlipBooks)
+	TArray<TObjectPtr<UPaperFlipbook>> FB_Shoot;
 	
 	virtual void BeginPlay() override;
+	void ChangeFlipBookAnimation();
 
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	UPROPERTY()
+	FVector2D Direction {};
+
+	UPROPERTY()
+	EDirectionFacing DirectionFacing {};
+	
 	UFUNCTION()
-	void Move(const FInputActionValue& Value);
+	void MoveTriggered(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void MoveCompleted(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void Shoot(const FInputActionValue& Value);
+
+	UFUNCTION()
+	EDirectionFacing GetDirectionFacing(const FVector2D& Value);
+
 };

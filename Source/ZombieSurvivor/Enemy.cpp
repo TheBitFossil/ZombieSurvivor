3205ce5db844a381875ed8de0a73213003885416
TheBitFossil <8717392-gearguard@users.noreply.gitlab.com>
2024-07-16 17:45:48 +0200

    facing direction movement

--- Source/ZombieSurvivor/Enemy.cpp
+++ Source/ZombieSurvivor/Enemy.cpp
@@ -3,25 +3,98 @@
 
 #include "Enemy.h"
 
-// Sets default values
+#include "Kismet/GameplayStatics.h"
+
 AEnemy::AEnemy()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Comp"));
+	SetRootComponent(CapsuleComponent);
+
+	FlipBookComponent = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("Flipbook"));
+	FlipBookComponent->SetupAttachment(RootComponent);
 }
 
-// Called when the game starts or when spawned
 void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Enemies should have the player from start
+	if(PlayerTarget == nullptr)
+	{
+		if(AActor* Actor = UGameplayStatics::GetActorOfClass(GetWorld(), ATopDownCharacter::StaticClass()))
+		{
+			PlayerTarget = static_cast<ATopDownCharacter*>(Actor);
+			//DrawDebugLine(GetWorld(), GetActorLocation(),PlayerTarget->GetActorLocation(), FColor::Red, true);
+		}
+	}
 }
 
-// Called every frame
 void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	CalculateFacingDirection();
+	
+	if(bIsAlive && PlayerTarget)
+	{
+		FVector Location = GetActorLocation();
+		FVector PlayerLocation =  PlayerTarget->GetActorLocation();
+		Direction =  PlayerLocation - Location;
+		//DrawDebugLine(GetWorld(), Location,PlayerLocation, FColor::Blue, false, .2f);
+		
+		DistanceToTarget = Direction.Size();
+		if(DistanceToTarget >= StoppingDistance)
+		{
+			FVector NormalizedDirection = Direction.GetSafeNormal();
+			FVector Velocity = Location + NormalizedDirection * MoveSpeed * DeltaTime;
+			SetActorLocation(Velocity);
+		}
+	}
+}
 
+EDirectionFacing AEnemy::CalculateFacingDirection()
+{
+	// Setting the scale to flip the Sprite
+	// FVector Scale = FlipBook->GetComponentScale();
+	// if(Scale.X > 0.f)
+	// FlipBook->SetWorldScale3D(FVector(-1.f, 1.f, 1.f));
+
+	FVector ForwardVector = GetActorForwardVector();
+	FVector UpVector = GetActorUpVector();
+	
+	GEngine->AddOnScreenDebugMessage(-1, .2f, FColor::Green, *ForwardVector.ToString());
+	
+	float DotForward = FVector::DotProduct(ForwardVector, Direction);
+	GEngine->AddOnScreenDebugMessage(-1, .2f, FColor::Green, FString::Printf(TEXT("DotF: %f"), DotForward));
+
+	float DotUp = FVector::DotProduct(UpVector, Direction);
+	GEngine->AddOnScreenDebugMessage(-1, .2f, FColor::Green, FString::Printf(TEXT("DotU: %f"), DotUp));
+
+	// Check if the direction to the player is mostly forward/backward or right/left
+	if (FMath::Abs(DotForward) > FMath::Abs(DotUp))
+	{
+		if(DotForward > 0.f)
+		{
+			DirectionFacing = EDirectionFacing::RIGHT;
+		}
+		else
+		{
+			DirectionFacing = EDirectionFacing::LEFT;
+		}
+	}
+	else
+	{
+		if(DotUp > .0f)
+		{
+			DirectionFacing = EDirectionFacing::UP;
+		}
+		else
+		{
+			DirectionFacing = EDirectionFacing::DOWN;
+		}
+	}
+
+	return DirectionFacing;
 }
 
--- Source/ZombieSurvivor/Enemy.h
+++ Source/ZombieSurvivor/Enemy.h
@@ -3,8 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PaperFlipbookComponent.h"
+#include "TopDownCharacter.h"
+#include "Components/CapsuleComponent.h"
 #include "GameFramework/Actor.h"
 #include "Enemy.generated.h"
+
 
 UCLASS()
 class ZOMBIESURVIVOR_API AEnemy : public AActor
@@ -12,15 +16,52 @@ class ZOMBIESURVIVOR_API AEnemy : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AEnemy();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TObjectPtr<UCapsuleComponent> CapsuleComponent;
 
-public:	
-	// Called every frame
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TObjectPtr<UPaperFlipbookComponent> FlipBookComponent;
+
+	// UP/ DOWN / RIGHT / LEFT
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=FlipBooks)
+	TArray<TObjectPtr<UPaperFlipbook>> FB_Idle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=FlipBooks)
+	TArray<TObjectPtr<UPaperFlipbook>> FB_Walk;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=FlipBooks)
+	TArray<TObjectPtr<UPaperFlipbook>> FB_Death;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=FlipBooks)
+	TArray<TObjectPtr<UPaperFlipbook>> FB_Attack;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TObjectPtr<ATopDownCharacter> PlayerTarget;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool bIsAlive{true};
+		
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float MoveSpeed{5.f};
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float StoppingDistance{20.f};
+
+	UPROPERTY(VisibleAnywhere)
+	float DistanceToTarget{};
+
+	UPROPERTY(VisibleAnywhere)
+	FVector Direction{};
+	
+	UPROPERTY(VisibleAnywhere)
+	EDirectionFacing DirectionFacing {};
+	
+	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	EDirectionFacing CalculateFacingDirection();
+	
 };
